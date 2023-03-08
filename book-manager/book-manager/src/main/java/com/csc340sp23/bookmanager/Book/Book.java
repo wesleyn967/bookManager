c8969e0fdd2262427534373adaa6d8d6c9675337
Wesley Nguyen <122545394+wesleyn967@users.noreply.github.com>
2023-03-08 00:19:51 -0500

    Add files via upload

---
+++ book-manager/book-manager/src/main/java/com/csc340sp23/bookmanager/Book/Book.java
@@ -0,0 +1,14 @@
+package com.csc340sp23.bookmanager.Book;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Wesley
+ */
+public class Book {
+    
+}
---
+++ book-manager/book-manager/src/main/java/com/csc340sp23/bookmanager/BookController/BookManagerController.java
@@ -0,0 +1,55 @@
+package com.csc340sp23.bookmanager.BookController;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+import com.csc340sp23.bookmanager.Book.Book;
+import com.example.bookstore.entity.Book;
+import com.example.bookstore.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/books")
+public class BookManagerController {
+
+    @Autowired
+    private BookService bookService;
+
+    @GetMapping
+    public List<Book> getAllBooks() {
+        return bookService.getAllBooks();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Book> getBookById(@PathVariable Long id) {
+        Optional<Book> optionalBook = bookService.getBookById(id);
+
+        if (optionalBook.isPresent()) {
+            return ResponseEntity.ok(optionalBook.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<Book> createBook(@RequestBody Book book) {
+        Book savedBook = bookService.createBook(book);
+        return new ResponseEntity<>(savedBook, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Book> updateBook(@PathVariable Long id, @RequestBody Book book) {
+        Book updatedBook = bookService.updateBook(id, book);
+
+        if (updatedBook != null) {
+            return ResponseEntity.ok(updatedBook);
+        } else {
+            return ResponseEntity.notFound().build();
